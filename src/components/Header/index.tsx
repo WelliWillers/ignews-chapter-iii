c9c857685063d25e63fe0b94913968adca1c200b
Wellington Willers <wellington.willers@gmail.com>
2021-09-16 21:36:06 -0300

    prismic - listagem de post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,8 +9,8 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="" >Home</a>
-          <a href="" >Post</a>
+          <a className={styles.active} href="/" >Home</a>
+          <a href="/posts" >Posts</a>
         </nav>
 
         <SignInButton />
